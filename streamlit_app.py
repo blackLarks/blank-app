cb219c8857ecc0872b062f87983f946e6b836cfd
Laurent <141468199+blackLarks@users.noreply.github.com>
2026-06-06 15:00:22 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 SOP on Poincaré sphere")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
